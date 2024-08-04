31044f3c386f31b94f583918ab81c57322505bad
Mihail <vislapyz@mail.ru>
2024-08-04 17:49:09 +0300

    модель Habits и миграция

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+# Эта команда загружает всё из файла .env
+load_dotenv(BASE_DIR / ".env")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "users",
+    "habits",
 ]
 
 MIDDLEWARE = [
--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class Habits(models.Model):
+    """Привычки и его свойства"""
+
+    owner = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        related_name="Привычки",
+        on_delete=models.CASCADE,
+        **NULLABLE,
+    )
+    place = models.CharField(
+        max_length=256,
+        verbose_name="Место",
+        help_text="Введите место выполнения привычки",
+        **NULLABLE,
+    )
+    time = models.TimeField(
+        verbose_name="Время", help_text="Введите время выполнения привычки"
+    )
+    action = models.CharField(
+        max_length=256,
+        verbose_name="Действие",
+        help_text="Введите действие, которое нужно выполнить",
+    )
+    is_pleasant_habit = models.BooleanField(
+        default=False, verbose_name="Признак приятной привычки"
+    )
+    associated_habit = models.ForeignKey(
+        "self",
+        verbose_name="Связанная приятная привычка",
+        on_delete=models.SET_NULL,
+        help_text="Данные признак указывается только для Полезной привычки",
+        **NULLABLE,
+    )
+    periodicity = models.PositiveSmallIntegerField(
+        default=1,
+        verbose_name="Периодичность",
+        help_text="Периодичность выполнения привычки для напоминания",
+    )
+    award = models.CharField(max_length=200, verbose_name="Вознаграждение", **NULLABLE)
+    execution_time = models.PositiveSmallIntegerField(
+        default=120,
+        verbose_name="Длительность выполнения",
+        help_text="Временя на выполнение полезной привычки в секундах ",
+    )
+    is_public = models.BooleanField(default=True, verbose_name="Признак публичности")
+
+    class Meta:
+        verbose_name = "Привычка"
+        verbose_name_plural = "Привычки"
+
+    def __str__(self):
+        return f"{self.owner} будет делать {self.action} в {self.time} в {self.place}"
