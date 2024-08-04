eba747565a527124bb910c4170c664dd40f8032b
Mihail <vislapyz@mail.ru>
2024-08-04 17:56:59 +0300

    Cоздания superuser

---
+++ users/management/commands/csu.py
@@ -0,0 +1,14 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    """Cоздания superuser"""
+    def handle(self, *args, **kwargs):
+        user = User.objects.create(email="admin@sky.pro")
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True
+        user.set_password("admin")
+        user.save()
